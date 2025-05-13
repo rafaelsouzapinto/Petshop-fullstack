5fa44321a8de5aa999125ab51487350f6d1ae1de
Antony Rafael Souza Pinto <154285174+rafaelsouzapinto@users.noreply.github.com>
2025-05-13 11:18:13 -0300

    inicio da página de registro de serviços

---
+++ frontend-petshop/src/pages/RegistrationPage.tsx
@@ -0,0 +1,13 @@
+import "../styles/registration-page.css";
+
+export default function RegistrationPage() {
+  return (
+    <>
+      <div className="container">
+        <div className="form-container">
+          <div className="form"> </div>
+        </div>
+      </div>
+    </>
+  );
+}
