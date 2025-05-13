7b8196e194eab0b4070ab3b13aa94d0f87a5497b
Antony Rafael Souza Pinto <154285174+rafaelsouzapinto@users.noreply.github.com>
2025-05-13 15:13:00 -0300

    amount-form

--- frontend-petshop/src/pages/RegistrationPage.tsx
+++ frontend-petshop/src/pages/RegistrationPage.tsx
@@ -1,3 +1,10 @@
+import {
+  FilledInput,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  Typography,
+} from "@mui/material";
 import "../styles/registration-page.css";
 
 export default function RegistrationPage() {
@@ -5,7 +12,38 @@ export default function RegistrationPage() {
     <>
       <div className="container">
         <div className="form-container">
-          <div className="form"> </div>
+          <div className="form">
+            <div className="form-title">
+              <Typography variant="h6" component="h2" color="textSecondary">
+                Cadastro de consultas
+              </Typography>
+            </div>
+            <div className="amount-form">
+              <FormControl
+                fullWidth
+                sx={{
+                  m: 1,
+                  width: "92ch",
+                  background: "#ffffff",
+                  borderRadius: "5px",
+                }}
+                variant="filled"
+              >
+                <InputLabel htmlFor="filled-adornment-amount">
+                  Amount
+                </InputLabel>
+                <FilledInput
+                  id="filled-adornment-amount"
+                  startAdornment={
+                    <InputAdornment position="start">$</InputAdornment>
+                  }
+                />
+              </FormControl>
+            </div>
+            <div className="service-status-form"></div>
+            <div className="pet-form"></div>
+            <div className="service-form"></div>
+          </div>
         </div>
       </div>
     </>
